e9f7ce436cfcc719dd7d1b7cc67087d037414238
eheath30 <elliot.heath@btinternet.com>
2022-09-09 18:31:52 +0100

    first commit

---
+++ components/Square.tsx
@@ -0,0 +1,18 @@
+type Player = 'X' | 'O' | 'BOTH' | null;
+
+function Square({
+    value, onClick, winner
+}: {
+    winner: Player;
+    value: Player;
+    onClick: () => void;
+}){
+    if (!value) {
+        return <button onClick={onClick} className="square" disabled={Boolean(winner)}/>
+    }
+    return <button
+    className={`square square_${value.toLowerCase()}`}
+    disabled>{value}</button>
+}
+
+export default Square
